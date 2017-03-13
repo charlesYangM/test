ddc26d36efc081526501c6bc08985d33c72b01bb
charlesYangM <qazwer1314123@gmail.com>
2017-03-13 15:34:43 +0800

    add the dynamical feature to the url creation

--- hello.py
+++ hello.py
@@ -2,9 +2,27 @@
 from flask import Flask
 app = Flask(__name__)
 
+# @app.route('/')
+# def hello_world():
+#     return 'Hello 张添 你是大美女!'
+
 @app.route('/')
-def hello_world():
-    return 'Hello 张添!'
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
